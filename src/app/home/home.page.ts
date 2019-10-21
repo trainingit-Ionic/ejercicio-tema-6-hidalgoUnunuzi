0bc95aa501513f2c11dabb8f8c76dcc0a3fcb0ed
CapiHidalgo <alvaro.hidalgo@ununuzi.es>
2019-10-21 18:16:52 +0200

    list user name and company name

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import { User } from '../model/user';
+import { UserService } from '../services/user.service';
+import { Observable } from 'rxjs';
+import { ModalController } from '@ionic/angular';
+import { CardPage } from '../card/card.page';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  users: User[];
+  displayData: any;
+  constructor(
+    private userService: UserService,
+    private modalController: ModalController,
+  ) {
+    this.userService.get().subscribe((res) => {
+      this.users = res;
+      console.log(this.users);
+     });
+  }
+/*
+  itemClick(itemId: any) {
+    this.userService.getOne(itemId).subscribe((res => {}))
 
+  }*/
+
+  ngOnInit() {}
 }
